91676e62f6db69ab268dd93fd1606e11bcaa29a1
Cliff Li <lic@student.ethz.ch>
2017-09-12 21:48:06 +0200

    meteor default structure

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// Client entry point, imports all client code
 
-import './main.html';
+import '/imports/startup/client';
+import '/imports/startup/both';
